384b778781defedd0ad81cd1e78e0833f58b5763
goku7 <goku7@DESKTOP-3T9BRKB>
2019-06-26 09:42:35 -0700

    Forgot to push last time

--- BFS/src/BFS4.java
+++ BFS/src/BFS4.java
@@ -1,9 +1,67 @@
 //1697
+import java.util.*;
+import java.io.*;
 public class BFS4 {
-
+	
+	static Scanner in;
+	static int a[], n, m;
+	static Queue<Integer> q;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		init();
+		solve();
+		
+	}
+	
+	public static void init() {
+		in = new Scanner(System.in);
+		n = in.nextInt();
+		m = in.nextInt();
+		a = new int[50];
+		q = new LinkedList<Integer>();
+		a[n] = 1;
+	}
+	
+	public static void solve() {
+		bfs(n);
+		//System.out.println(Arrays.toString(a));
+	}
+	
+	public static void bfs(int x) {
+		int ans = 0;
+		q.add(x);
+		while(!q.isEmpty()) {
+			x = q.poll();
+			
+			//System.out.println(q);
+			if(x==m) {
+				ans = a[x]-1;
+				break;
+			}
+			int nx = 0;
+			
+			nx = x + 1;
+			//System.out.println(nx);
+			if(nx>=0&&nx<=49&&a[nx]==0) {
+				a[nx] = a[x]+1;
+				q.add(nx);
+			}
+			
+			nx = x - 1;
+			if(nx>=0&&nx<=49&&a[nx]==0) {
+				a[nx] = a[x]+1;
+				q.add(nx);
+			}
+			
+			nx = x * 2;
+			if(nx>=0&&nx<=49&&a[nx]==0) {
+				a[nx] = a[x]+1;
+				q.add(nx);
+			}
+			
+		}
+		System.out.println(ans);
 	}
 
 }
--- BFS/src/BFS5.java
+++ BFS/src/BFS5.java
@@ -1,9 +1,85 @@
 //7576 Tomato - Adjacency Graph
+import java.util.*;
+import java.io.*;
+
 public class BFS5 {
-
+	
+	static Scanner in;
+	static int a[][], n, m;
+	static int dx[] = {-1,1,0,0};
+	static int dy[] = {0,0,-1,1};
+	static Queue<Integer> q;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		try {
+			init();
+			solve();
+		}
+		catch(Exception e){
+			System.out.println("Bruh you stoopid");
+			e.printStackTrace();
+		}
 	}
+	
+	public static void init() {
+		in = new Scanner(System.in);
+		m = in.nextInt();
+		n = in.nextInt();
+		q = new LinkedList<Integer>();
+		in.nextLine();
+		a = new int[n][m];
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < m; j++) {
+				a[i][j] = in.nextInt();
+				if(a[i][j]==1) {
+					q.add(i);
+					q.add(j);
+				}
+			}
+		}
+	}
+	
+	public static void solve() {
+		bfs();
+		int count = 0;
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < m; j++) {
+				count = Math.max(count, a[i][j]);
+			}
+		}
+		int zeroes = 0;
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < m; j++) {
+				if(a[i][j]==0) zeroes++;
+			}
+		}
+		if(zeroes>0) {
+			System.out.println(-1);
+		}
+		else {
+			System.out.println(count-1);
+		}
+		System.out.println(Arrays.deepToString(a));
+	}
+	
+	public static void bfs() {
+		while(!q.isEmpty()) {
+			int x = q.poll();
+			int y = q.poll();
+			for(int i = 0; i < 4; i++) {
+				int nx = x + dx[i];
+				int ny = y + dy[i];
+				if(nx<0||nx>=n||ny<0||ny>=m)
+					continue;
+				if(a[nx][ny]==0) {
+					q.add(nx);
+					q.add(ny);
+					a[nx][ny] = a[x][y]+1;
+				}
+			}
+		}
+	}
+	
+	
 
 }
